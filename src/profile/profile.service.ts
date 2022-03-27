35e8b39edf5f625ee50cc5742ae2fa7f4fb22e29
EgorkaPl <egorplatonov2002@gmail.com>
2022-03-27 14:17:16 +0300

    'profile/my-profile'

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -1,17 +1,23 @@
 import { HttpCode, HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { Request } from 'express';
+import { UserService } from 'src/users/users.service';
 
 @Injectable()
 export class ProfileService {
 
-    constructor(private jwtService: JwtService) {}
+    constructor(private jwtService: JwtService, private userService: UserService) {}
     
     async getMyProfile(request: Request) {
         const BearerToken: any = request.headers.authorization;
         const token = BearerToken.split(' ')[1];
-        const decodedToken = this.jwtService.decode(token);
-        console.log(decodedToken);
-        throw new HttpException('Gotovo', 200)
+        const decodedToken: any = this.jwtService.decode(token);
+
+        const user = await this.userService.getUserByEmail(decodedToken.email);
+        if(user) {
+            throw new HttpException(user, 200)
+        } else {
+            throw new HttpException('Ошибка. Обновите токен.', HttpStatus.UNAUTHORIZED);
+        }
     }
 }
