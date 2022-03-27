a2df706859d8c5c51859d7ba2287114f0a164827
EgorkaPl <egorplatonov2002@gmail.com>
2022-03-27 14:22:18 +0300

    'profile/mypRrofile'

--- src/profile/profile.module.ts
+++ src/profile/profile.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { ProfileService } from './profile.service';
 import { ProfileController } from './profile.controller';
 import { AuthModule } from 'src/auth/auth.module';
+import { UsersModule } from 'src/users/users.module';
 
 @Module({
   providers: [ProfileService],
   controllers: [ProfileController],
-  imports: [AuthModule]
+  imports: [AuthModule, UsersModule]
 })
 export class ProfileModule {}
