52f651ce090b34abe3c5e993fdd985ec1103c229
EgorkaPl <egorplatonov2002@gmail.com>
2022-03-27 13:52:35 +0300

    'profile22'

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -8,16 +8,12 @@ export class ProfileService {
     constructor(private jwtService: JwtService) {}
     
     async getMyProfile(request: Request) {
-        try {
-            const BearerToken: any = request.headers.authorization;
-            const token = BearerToken.split(' ')[1];
-            //const decodedToken = this.jwtService.decode(token);
-            //console.log(request.headers);
-            console.log(token);
-            //console.log(decodedToken);
-            throw new HttpException('Gotovo', 200)
-        } catch(e) {
-            throw new HttpException('Невалидный токен. Обновите.', HttpStatus.UNAUTHORIZED)
-        }
+        const BearerToken: any = request.headers.authorization;
+        const token = BearerToken.split(' ')[1];
+        //const decodedToken = this.jwtService.decode(token);
+        //console.log(request.headers);
+        console.log(token);
+        //console.log(decodedToken);
+        throw new HttpException('Gotovo', 200)
     }
 }
