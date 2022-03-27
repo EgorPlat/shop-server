8ae6d3070bc47dca0cd31359381eac51be50d602
EgorkaPl <egorplatonov2002@gmail.com>
2022-03-27 13:56:01 +0300

    'decodeToken'

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -10,10 +10,8 @@ export class ProfileService {
     async getMyProfile(request: Request) {
         const BearerToken: any = request.headers.authorization;
         const token = BearerToken.split(' ')[1];
-        //const decodedToken = this.jwtService.decode(token);
-        //console.log(request.headers);
-        console.log(token);
-        //console.log(decodedToken);
+        const decodedToken = this.jwtService.decode(token);
+        console.log(decodedToken);
         throw new HttpException('Gotovo', 200)
     }
 }
