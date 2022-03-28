9b30248d46de7386d00165521fbedc3052821a71
EgorkaPl <egorplatonov2002@gmail.com>
2022-03-28 10:47:42 +0300

    changeStatic

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,8 @@ const start = async () => {
     try {
         const PORT = process.env.PORT || 5000;
         const app = await NestFactory.create<NestExpressApplication>(AppModule);
-        app.useStaticAssets(join(__dirname, '../../server/dist'));
+        app.useStaticAssets(join(__dirname, '../../server/dist')); 
+        // статика
         app.enableCors({
             origin: true,
             methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
