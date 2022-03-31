9481951fb1d3357ad8b55164e04aaa41fef9ede2
EgorkaPl <egorplatonov2002@gmail.com>
2022-03-31 09:24:31 +0300

    staticc

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import {AppModule} from "./app.module";
 
 
 const start = async () => {
-    try {
+    try {// статика
         const PORT = process.env.PORT || 5000;
         const app = await NestFactory.create<NestExpressApplication>(AppModule);
         app.useStaticAssets(join(__dirname, './dist')); 
