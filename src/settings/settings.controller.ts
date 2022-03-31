1334fc1dc32229d226369584755ff8d69cedf20b
EgorkaPl <egorplatonov2002@gmail.com>
2022-03-31 09:48:53 +0300

    settings

--- src/settings/settings.controller.ts
+++ src/settings/settings.controller.ts
@@ -7,6 +7,6 @@ export class SettingsController {
     constructor(private settingsService: SettingsService) {}
     @Post('/update-avatar')
     updateUserAvatar(req: Request) {
-        return this.settingsService.updateUserAvatar(req.body);
+        return this.settingsService.updateUserAvatar(req);
     }
 }
