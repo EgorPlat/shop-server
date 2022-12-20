3ac0336f89ce0fb1d52db3365a443549b236581b
EgorkaPl <egorplatonov2002@gmail.com>
2022-12-20 15:36:28 +0300

    add-online-user-profile-status

--- src/profile/profile.service.ts
+++ src/profile/profile.service.ts
@@ -6,16 +6,22 @@ import { UserService } from 'src/users/users.service';
 @Injectable()
 export class ProfileService {
 
-    constructor(private jwtService: JwtService, private userService: UserService) {}
+    constructor(
+        private jwtService: JwtService, 
+        private userService: UserService,
+        private socketServer: AppGateway
+    ) {}
     
     async getMyProfile(request: Request) {
         const BearerToken: any = request.headers.authorization;
         const token = BearerToken.split(' ')[1];
         const decodedToken: any = this.jwtService.decode(token);
-
         const user = await this.userService.getUserByEmail(decodedToken.email);
-        if(user) {
-            throw new HttpException(user, 200)
+        const isUserOnline: boolean = this.socketServer.activeUsersList.includes(user.email);
+        
+        const advanceUser = {...user, isOnline: isUserOnline};
+        if(advanceUser) {
+            throw new HttpException(advanceUser, 200)
         } else {
             throw new HttpException('Ошибка. Обновите токен.', HttpStatus.UNAUTHORIZED);
         }
